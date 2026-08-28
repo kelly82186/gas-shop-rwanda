c9b07ab7e62035beff296389ca09f579a9b38eec
kelly82186 <gkbeen9@gmail.com>
2026-08-28 20:29:22 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,17 +12,17 @@ const app = express();
 const port = Number(process.env.PORT || 5000);
 
 console.log("ENV CHECK:");
-console.log("DB_HOST:", process.env.DB-HOST);
-console.log("DB_PORT:", process.env.DB-PORT);
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
 console.log("DB_USER:", process.env.root);
 console.log("DB_NAME:", process.env.gas_sho_db);
 
 const db = mysql.createPool({
   host: process.env.mysql.railway.internal,
-  port: Number(process.env.DB-PORT),
-  user: process.DB-USER,
-  password: process.env.DB-PASSWORD,   // ✅ matches .env
-  database: process.env.DB-NAME,   // ✅ use env variable
+  port: Number(process.env.DB_PORT),
+  user: process.DB_USER,
+  password: process.env.DB_PASSWORD,   // ✅ matches .env
+  database: process.env.DB_NAME,   // ✅ use env variable
   connectionLimit: 10,              // ✅ no stray text
 })
 

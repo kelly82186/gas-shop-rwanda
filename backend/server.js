74ca142ce98d57a1e6ed2c6f7251f7e3b5778405
kelly82186 <gkbeen9@gmail.com>
2026-08-28 16:51:32 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,20 +10,18 @@ dotenv.config();
 
 const app = express();
 const port = Number(process.env.PORT || 5000);
-const databaseName = process.env.DB_NAME || "gas_shop_db";
-const googleClientId = process.env.GOOGLE_CLIENT_ID;
-const googleClient = new OAuth2Client(googleClientId);
-
-app.use(cors());
-app.use(express.json());
+console.log("ENV CHECK:");
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  port: Number(process.env.DB_PORT || 3306),
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: databaseName,
-  waitForConnections: true,
+host: process.env.DB_HOST,
+port: Number(process.env.DB_PORT),
+user: process.env.DB_USER,
+password: process.env.DB_PASSWORD,
+database: process.env.DB_NAME,
   connectionLimit: 10,
 }).promise();
 
